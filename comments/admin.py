7b645088667698681349c8256e4d344246b24bfa
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-02-18 23:23:48 +0200

    Update CommentAdmin in admin.py.

--- comments/admin.py
+++ comments/admin.py
@@ -4,11 +4,11 @@ from django.contrib.auth.admin import UserAdmin
 from comments.models import Comment, User
 
 
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ["user", "text", "created_at", "parent_comment", ]
     list_filter = ["user", "created_at", ]
     search_fields = ["user__username", ]
 
 
-admin.site.register(Comment, CommentAdmin)
 admin.site.register(User, UserAdmin)
