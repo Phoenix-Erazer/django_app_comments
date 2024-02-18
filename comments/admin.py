dc757e82fda3d95d44cb731a87a217eb38eb5e0b
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-02-18 23:11:44 +0200

    Add CommentAdmin in admin.py.

--- comments/admin.py
+++ comments/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from comments.models import Comment
+from comments.models import Comment, User
 
-# Register your models here.
-admin.site.register(Comment)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["user", "text", "created_at", "parent_comment", ]
+    list_filter = ["user", "created_at", ]
+    search_fields = ["user__username", ]
+
+
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(User, UserAdmin)
