cff2296ba2da7548d1f56bc9e71e4ee528deda7d
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-02-18 21:32:55 +0200

    Add models User and Comment in models.py

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class User(AbstractUser):
+    homepage = models.URLField(blank=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    parent_comment = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
