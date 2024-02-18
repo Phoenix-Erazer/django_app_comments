92e67f8f8e14cd561d88a5399941e30485eef9fa
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-02-18 21:40:30 +0200

    Fix Comment in models.py

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -9,7 +10,10 @@ class User(AbstractUser):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     parent_comment = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.name}: {self.text}"
