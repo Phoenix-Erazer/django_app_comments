7d987aa4f574d7a0c39c5040854b6056394e5805
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-02-21 22:52:21 +0200

    Fix views.py

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -9,33 +7,15 @@ from .forms import CommentForm
 
 class CommentCreateView(generic.CreateView):
     form_class = CommentForm
-    success_url = reverse_lazy("comments:home")
-    template_name = "includes/add_comment.html"
-
-
-# def add_comment(request):
-#     if request.method == "POST":
-#         form = CommentForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect("comments:home")
-#     else:
-#         form = CommentForm()
-#     return render(request, "includes/add_comment.html", {"form": form})
+    success_url = reverse_lazy("comments:comment_list")
+    template_name = "comments/add_comment.html"
 
 
 class CommentListView(generic.ListView):
     model = Comment
-    template_name = "includes/comment_list.html"
+    template_name = "comments/comment_list.html"
     context_object_name = "comment_list"
     paginate_by = 25
 
-    queryset = Comment.objects.select_related("parent_comment")\
-        .order_by("-created_at")
-
-
-def home(request):
-    comments = Comment.objects.filter(parent_comment__isnull=True).order_by(
-        "-created_at"
-    )
-    return render(request, "comments/home.html", {"comment_list": comments})
+    queryset = Comment.objects.filter(parent_comment__isnull=True)\
+        .prefetch_related("comments").order_by("created_at")
