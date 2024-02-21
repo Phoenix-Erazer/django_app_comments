ecb27477f4d999156757be01a6903c8ef3e4f046
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-02-21 13:13:58 +0200

    fix views.py

--- comments/forms.py
+++ comments/forms.py
@@ -1,24 +1,27 @@
 from django import forms
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
 from captcha.fields import CaptchaField
-
 from comments.models import Comment
 
 
-class CommentForm(forms.Form):
-    username = forms.CharField(
-        label="User Name",
-        max_length=50,
-        required=True,
-        help_text="Only letters and digits are allowed.",
-    )
-    email = forms.EmailField(label="E-mail", required=True)
-    homepage = forms.URLField(label="Home page", required=False)
+class CommentForm(forms.ModelForm):
     captcha = CaptchaField()
-    text = forms.CharField(label="Text", widget=forms.Textarea, required=True)
-    parent_comment = forms.ModelChoiceField(
-        queryset=Comment.objects.select_related('user').order_by("-created_at"),
-        required=False,
-        empty_label="Select parent comment (optional)",
-    )
+
+    class Meta:
+        model = Comment
+        fields = [
+            "user_name",
+            "email",
+            "home_page",
+            "text",
+            "parent_comment"
+        ]
+        labels = {
+            "user_name": "User Name",
+            "email": "E-mail",
+            "home_page": "Home Page",
+            "text": "Text",
+            "parent_comment": "Parent Comment"
+        }
+        help_texts = {
+            "parent_comment": "Select parent comment (optional)"
+        }
