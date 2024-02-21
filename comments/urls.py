acb987621cd8d616424667da94e4e40f6c761fd1
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-02-21 22:51:50 +0200

    Delete home.html

--- comments/urls.py
+++ comments/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import CommentListView, home, CommentCreateView
+from .views import CommentListView, CommentCreateView
 
 urlpatterns = [
     path("add/", CommentCreateView.as_view(), name="add_comment"),
     path("comment-list/", CommentListView.as_view(), name="comment_list"),
-    path("", home, name="home"),
 ]
 
 app_name = "comments"
