f1712b0d789a359e3851cc42c939b6613d4cb599
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-02-21 14:46:26 +0200

    Adding crispy forms to a project

--- django_app_comments/settings.py
+++ django_app_comments/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "comments",
     "captcha",
 ]
@@ -134,3 +136,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
