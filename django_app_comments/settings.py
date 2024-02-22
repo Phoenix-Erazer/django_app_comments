b406886a06aed111c59e54fc6e99c6b999f8bece
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-02-22 15:39:21 +0200

    Add https://django-app-comments.onrender.com in settings.py/ALLOWED_HOSTS

--- django_app_comments/settings.py
+++ django_app_comments/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "django-app-comments.onrender.com"
+]
 
 
 # Application definition
